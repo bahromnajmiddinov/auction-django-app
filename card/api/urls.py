7bf3c4c200eb793e124133187c99886aa7989328
Bahrom <najmiddinovbahrom402@gmail.com>
2024-07-18 16:33:24 +0500

    Is User saved Item or not to Card functionality added

--- card/api/urls.py
+++ card/api/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.CartDetailApiView.as_view(), name='cart-detail-api'),
     path('add/<slug:slug>/', views.AddToCartApiView.as_view(), name='add-to-cart-api'),
+    path('is-saved/<slug:slug>/', views.is_item_saved, name='is-item-saved-api'),
 ]
 
